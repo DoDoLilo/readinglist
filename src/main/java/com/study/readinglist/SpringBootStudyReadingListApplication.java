136961f8deca74229c2369b9a2ecef95d7b6d4d0
DoDoLilo <1479953662@qq.com>
2022-05-03 22:43:51 +0800

    Run succeed but not understand completely.

--- src/main/java/com/study/readinglist/SpringBootStudyReadingListApplication.java
+++ src/main/java/com/study/readinglist/SpringBootStudyReadingListApplication.java
@@ -2,12 +2,19 @@ package com.study.readinglist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class SpringBootStudyReadingListApplication {
+public class SpringBootStudyReadingListApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootStudyReadingListApplication.class, args);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/readingList");
     }
 
 }
